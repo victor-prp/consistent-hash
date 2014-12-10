c833f07e79310aaab7510a9b64d0d3a400b28a63
Victor Perepelitsky <victor.prp@gmail.com>
2014-12-10 18:48:25 +0200

    code organization

--- src/test/java/victor/prp/consistent/hash/ConsistentHashTest.java
+++ src/test/java/victor/prp/consistent/hash/ConsistentHashTest.java
@@ -6,6 +6,7 @@ import org.junit.Test;
 import java.util.*;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.concurrent.atomic.AtomicLong;
+import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 /**
@@ -15,98 +16,91 @@ public class ConsistentHashTest {
 
 
     @Test
-    public void validateRehashingOnAdd(){
+    public void validateRebalancingWhenNodeIsAdded(){
         int NODES_COUNT = 10;
         int BUCKETS_COUNT =  100;
         int KEYS_COUNT = 1000000;
 
+        final Set<String> keys = createRandomKeys(KEYS_COUNT);
+
         /**
          * Phase 1
-         * Simulate NODES_COUNT with KEYS_COUNT
+         * Simulate with random nodes and random keys
          */
-        final Map<String,AtomicInteger> nodes_init = initNodesRandomly(NODES_COUNT);
-        final Map<String,Set<String>> node2keys_init = new HashMap<>();
-        final Map<String,Set<String>> node2keys_after = new HashMap<>();
-
-        nodes_init.forEach((node,keysCount)-> {
-            node2keys_init.put(node,new HashSet<>());
-            node2keys_after.put(node,new HashSet<>());
-        });
-
-        ConsistentHash consistentHash = new ConsistentHash(BUCKETS_COUNT,nodes_init.keySet());
-
-        IntStream.rangeClosed(1,KEYS_COUNT)
-                .forEach(k->{
-                    String randomKey  = UUID.randomUUID().toString();
-                    String node = consistentHash.calculateNode(randomKey);
-                    Assert.assertTrue("algo returned non existing node",nodes_init.containsKey(node));
-                    nodes_init.get(node).incrementAndGet();
-                    node2keys_init.get(node).add(randomKey);
-                });
-
-        nodes_init.forEach((node,count)-> {
-            System.out.println("node: " + node + " count: " + count.get() + " key size: " + node2keys_init.get(node).size());
-
-        });
-
-        int sum = nodes_init.values()
-                .stream()
-                .reduce((l, r) -> new AtomicInteger(l.get() + r.get())).get().get();
-        System.out.println("sum: " + sum);
-
-        System.out.println("adding new node to the system..");
-
-
-        consistentHash.addNode("new-node");
-        node2keys_after.put("new-node",new HashSet<>());
+        final Map<String,AtomicInteger> nodesPhase1 = initNodesRandomly(NODES_COUNT);
+        Map<String,Set<String>> nodes2KeysPhase1 = simulate(BUCKETS_COUNT, nodesPhase1, keys);
 
         /**
          * Phase 2
-         * Simulate NODES_COUNT + 1 with KEYS_COUNT using the same keys from phase 1
+         * Simulate with same nodes from phase 1 + "new-node" and with same keys from phase 1
          */
-        final Map<String,AtomicInteger> nodes_after = new HashMap<>();
-        nodes_after.putAll(nodes_init);
-        nodes_after.put("new-node", new AtomicInteger());
-        nodes_after.forEach((node,count)->count.set(0));
+        final Map<String,AtomicInteger> nodesPhase2 = initNodes(nodesPhase1.keySet(),"new-node");
+        Map<String,Set<String>> nodes2KeysPhase2 = simulate(BUCKETS_COUNT, nodesPhase2, keys);
 
-        node2keys_init.values()
-                .stream()
-                .flatMap(keys->keys.stream())
-                .forEach(key->{
-                    String node = consistentHash.calculateNode(key);
-                    Assert.assertTrue("algo returned non existing node", nodes_after.containsKey(node));
-                    node2keys_after.get(node).add(key);
-                    nodes_after.get(node).incrementAndGet();
-                });
-
-
-
-        nodes_after.forEach((node,count)-> {
-            System.out.println("node: " + node + " count: " + count.get() + " key size: " + node2keys_after.get(node).size());
-
-        });
-
-        sum = nodes_after.values()
-                .stream()
-                .reduce((l, r) -> new AtomicInteger(l.get() + r.get())).get().get();
-        System.out.println("sum: " + sum);
-
-
-        AtomicLong movedKeysCount = new AtomicLong(0);
         /**
          * Phase 3
          * Validate re-balancing was as expected
          */
-         node2keys_init.forEach((node,keys)->{
+        long movedKeysCountResult = calculatedMovedKeys(nodes2KeysPhase1, nodes2KeysPhase2);
+        System.out.println("moved (rebalanced) keys count: " + movedKeysCountResult);
+
+
+    }
+
+    private long calculatedMovedKeys(Map<String, Set<String>> nodes2KeysPhase1, Map<String, Set<String>> nodes2KeysPhase2) {
+        AtomicLong movedKeysCount = new AtomicLong(0);
+        nodes2KeysPhase1.forEach((node,keysInNode)->{
              long movedCount =
-                keys.stream()
-                    .filter(key -> !match(node2keys_after, node, key))
-                    .count();
+                     keysInNode.stream()
+                        .filter(key -> !match(nodes2KeysPhase2, node, key))
+                        .count();
              movedKeysCount.addAndGet(movedCount);
         });
-        System.out.println("moved (rebalanced) keys count: " + movedKeysCount.get());
 
+        return movedKeysCount.get();
+    }
 
+    private Map<String, AtomicInteger> initNodes(Set<String> nodes, String... moreNodes) {
+        Set<String> nodesToInit = new HashSet<>(nodes);
+        nodesToInit.addAll(Arrays.asList(moreNodes));
+        return nodesToInit.stream().collect(Collectors.toMap(node->node,node->new AtomicInteger()));
+    }
+
+    private Map<String,Set<String>> simulate(int bucketCount, Map<String, AtomicInteger> nodes, Set<String> keys) {
+        final Map<String,Set<String>> node2keys = initNodes2Keys(nodes.keySet());
+        ConsistentHash consistentHash = new ConsistentHash(bucketCount,nodes.keySet());
+
+        keys.stream()
+                .forEach(key -> {
+                    String node = consistentHash.calculateNode(key);
+                    Assert.assertTrue("algo returned non existing node", nodes.containsKey(node));
+                    nodes.get(node).incrementAndGet();
+                    node2keys.get(node).add(key);
+                });
+
+        nodes.forEach((node,count)-> {
+            System.out.println("node: " + node + " count: " + count.get() + " key size: " + node2keys.get(node).size());
+
+        });
+
+        int sum = nodes.values()
+                .stream()
+                .reduce((l, r) -> new AtomicInteger(l.get() + r.get())).get().get();
+        System.out.println("sum: " + sum);
+        return node2keys;
+    }
+
+    private static Map<String, Set<String>> initNodes2Keys(Set<String> nodes) {
+        final Map<String,Set<String>> node2keys = new HashMap<>(nodes.size());
+        nodes.forEach(node -> node2keys.put(node,new HashSet<>()));
+        return node2keys;
+    }
+
+    private static Set<String> createRandomKeys(int keysCount) {
+        return
+            IntStream.rangeClosed(1, keysCount)
+                .mapToObj(sequence -> UUID.randomUUID().toString())
+                .collect(Collectors.toSet());
     }
 
     private boolean match(Map<String, Set<String>> node2keys_after, String node, String key) {
